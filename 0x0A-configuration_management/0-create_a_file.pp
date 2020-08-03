712df1d6e0f003f550615036887985a8986b368b
cirine <1410@holbertonschool.com>
2020-08-03 19:45:49 +0100

    manifest to create a holberton file with specific attributes

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Using Puppet, create a file in /tmp.
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
