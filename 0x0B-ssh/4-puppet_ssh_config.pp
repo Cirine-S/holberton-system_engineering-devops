2c99844971e00d00211850ecac7aded8e1dd98ae
cirine <1410@holbertonschool.com>
2020-08-05 00:03:26 +0100

    up

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # Puppet config
-file_line { 'Declare identity file':
+file_line {'Use the private key':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
-file_line {'Turn off pwd auth':
+file_line {'Refuse pwd auth':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
