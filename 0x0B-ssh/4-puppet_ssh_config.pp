5bd300d24c40aad4c2131ba43cf3259b4b7e2d28
cirine <1410@holbertonschool.com>
2020-08-05 00:01:21 +0100

    up

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,7 +1,9 @@
 # Puppet config
-file_line { '/etc/ssh/ssh_config':
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
-file_line {'/etc/ssh/ssh_config':
+file_line {'Turn off pwd auth':
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
