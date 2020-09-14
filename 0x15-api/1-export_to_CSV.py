9f2bb2f79bd707d0d66c59e7f3c70f26bd1c58bc
cirine <1410@holbertonschool.com>
2020-09-15 00:19:05 +0100

    pep8

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,21 +5,22 @@ import requests
 import sys
 
 
-def to_csv(employee_id):
+def to_csv(empl_id):
     ''' Using a given REST API, for a given employee ID,
     returns information about his/her TODO list progress'''
 
     employee_name = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' + employee_id).json()['username']
+        'https://jsonplaceholder.typicode.com/users/' + empl_id).json()
+    employee_name = employee_name['name']
 
     info = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id).json()
+        'https://jsonplaceholder.typicode.com/todos?userId=' + empl_id).json()
 
-    with open('{}.csv'.format(employee_id), 'w') as csv_file:
+    with open('{}.csv'.format(empl_id), 'w') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in info:
             writer.writerow([
-                employee_id,
+                empl_id,
                 employee_name,
                 task.get('completed'),
                 task.get('title')
@@ -27,5 +28,5 @@ def to_csv(employee_id):
 
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    to_csv(employee_id)
+    empl_id = sys.argv[1]
+    to_csv(empl_id)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,29 +5,30 @@ import requests
 import sys
 
 
-def to_csv(employee_id):
+def to_csv(empl_id):
     ''' Using a given REST API, for a given employee ID,
     returns information about his/her TODO list progress'''
 
     employee_name = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' + employee_id).json()['username']
+        'https://jsonplaceholder.typicode.com/users/' + empl_id).json()
+    employee_name = employee_name['username']
 
     info = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id).json()
+        'https://jsonplaceholder.typicode.com/todos?userId=' + empl_id).json()
 
-    data = {employee_id: []}
+    data = {empl_id: []}
     for task in info:
         new_dic = {}
         new_dic["task"] = task.get('title')
         new_dic["completed"] = task.get('completed')
         new_dic["username"] = employee_name
-        data[employee_id].append(new_dic)
+        data[empl_id].append(new_dic)
 
     # write json
-    with open('{}.json'.format(employee_id), 'w') as f:
+    with open('{}.json'.format(empl_id), 'w') as f:
         json.dump(data, f)
 
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    to_csv(employee_id)
+    empl_id = sys.argv[1]
+    to_csv(empl_id)
