36b7bdd8e46dc75cd15b46ed22a569294be7ab70
cirine <1410@holbertonschool.com>
2020-09-18 00:06:21 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,9 @@
 """
 subreddit module
 """
-def number_of_subscribers(subreddit)
+
+
+def number_of_subscribers(subreddit):
     '''returns the number of suscribers'''
     header = {UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5)\
      AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'}
