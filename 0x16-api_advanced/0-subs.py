0c055fa6a761024091f4cd8892d5d5a530a866a7
cirine <1410@holbertonschool.com>
2020-09-18 01:49:28 +0100

    user-agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''returns the number of suscribers'''
-    header = {'User-agent': 'hbtn'}
+    header = {'User-agent': 'Cirine'}
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
 
     r = requests.get(url, headers=header)
