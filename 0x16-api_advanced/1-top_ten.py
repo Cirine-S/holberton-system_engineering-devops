cfa201ce7247f254946251f4ea48a09bac3e9195
cirine <1410@holbertonschool.com>
2020-09-18 02:00:39 +0100

    user-agent

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,13 +10,10 @@ def top_ten(subreddit):
     header = {'User-agent': 'Cirine'}
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
 
-    r = requests.get(url, headers=header, allow_redirects=False).json()
-    try:
-        for i in range(10):
-            path = r.get('data').get('children')
-            print(path[i].get('data').get('title'))
+    r = requests.get(url, headers=header)
+    if r.status_code != 200:
+        print(None)
         return
-    except Exception:
-        pass
-    print(None)
-    return
+    path = r.json().get('data').get('children')
+    for i in range(10):
+        print(path[i].get('data').get('title'))
