60fdac72e67ec5bacec61b7b1607fb6b3a6a6e25
cirine <1410@holbertonschool.com>
2020-10-08 22:48:53 +0100

    hbtn log

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Puppet manifest to login with the holberton user and open a file without any error message.
 
-exec {'holberton hard':
-path     => '/usr/bin/',
-command  => "sudo sed -i 's/^holberton hard /holberton hard nofile 2000/g' /etc/security/limits.conf",
+exec {'fix hard':
+path    => '/usr/bin/',
+command => "sudo sed -i 's/^holberton hard nofile.*/holberton hard nofile 2000/g' /etc/security/limits.conf",
 }
 
-exec {'holberton soft':
-path     => '/usr/bin/',
-command  => "sudo sed -i 's/^holberton soft /holberton soft nofile 2000/g' /etc/security/limits.conf",
+exec {'fix soft':
+path    => '/usr/bin/',
+command => "sudo sed -i 's/^holberton soft nofile.*/holberton soft nofile 2000/g' /etc/security/limits.conf",
 }
